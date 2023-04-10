2c0c9c14ccf4d2a47c0978298f151f4cbc4d50e4
zhyper <percy.hk@gmail.com>
2023-04-10 11:14:54 -0500

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  HOST: "144.91.84.249",
+  USER: "zhyper",
+  PASSWORD: "Zhyper*332011",
+  port:"3306",
+  DB: "41zre_prueba",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
